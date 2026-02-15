843340c098ee9ab4939d15d0329d1142186f4f65
Shawn Partain <g5.partain@gmail.com>
2026-02-15 15:22:29 -0800

    Add button feedback, motivational snippets, weekly exercise rotation

---
+++ sw.js
@@ -0,0 +1,25 @@
+/* Service Worker for Strength Levels PWA - enables offline caching */
+const CACHE_NAME = 'strength-levels-v1';
+const urlsToCache = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((names) =>
+      Promise.all(names.filter((n) => n !== CACHE_NAME).map((n) => caches.delete(n)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
